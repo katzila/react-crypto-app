ae96e7644e13ce4b081115d98dbd3e10054e40be
Andrey Lyakhov <3qandre17@gmail.com>
2022-02-18 22:52:45 +0700

    fix: disable no-unused-vars for that line

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+// eslint-disable-next-line no-unused-vars
 import { Chart as ChartJS } from 'chart.js/auto'
 import { Col, Row, Typography } from 'antd';
 
