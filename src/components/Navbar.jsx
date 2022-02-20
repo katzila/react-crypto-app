b0e695f7ccfb97fb4bf8d0d532af027520593f87
Andrey Lyakhov <3qandre17@gmail.com>
2022-02-20 12:45:58 +0700

    fix: correct selected Menu.item

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Button, Menu, Typography, Avatar } from 'antd';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons/lib/icons';
 
 import icon from '../images/cryptocurrency.png'
 
-const Navbar = ({ activeMenu, setActiveMenu}) => {
+const Navbar = ({ activeMenu, setActiveMenu }) => {
+    let location = useLocation()
 
     return (
         <div className='nav-container'>
             <div className='logo-container'>
                 <Avatar src={icon} size='large' />
                 <Typography.Title level={2} className='logo'>
-                    <Link to='/'>Cryptoverse</Link>
+                    <Link to='/' >Cryptoverse</Link>
                 </Typography.Title>
                 <Button className='menu-control-container' onClick={() => setActiveMenu(true)}>
                     <MenuOutlined />
@@ -20,17 +21,17 @@ const Navbar = ({ activeMenu, setActiveMenu}) => {
             </div>
 
             {activeMenu && (
-                <Menu theme='dark'>
-                    <Menu.Item icon={<HomeOutlined />}>
+                <Menu theme='dark' defaultSelectedKeys={['/']} selectedKeys={[location.pathname]}>
+                    <Menu.Item icon={<HomeOutlined />} key='/'>
                         <Link to='/'>Home</Link>
                     </Menu.Item>
-                    <Menu.Item icon={<FundOutlined />}>
+                    <Menu.Item icon={<FundOutlined />} key='/cryptocurrencies'>
                         <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
                     </Menu.Item>
-                    <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Menu.Item icon={<MoneyCollectOutlined />} key='/exchanges'>
                         <Link to='/exchanges'>Exchanges</Link>
                     </Menu.Item>
-                    <Menu.Item icon={<BulbOutlined />}>
+                    <Menu.Item icon={<BulbOutlined />} key='/news'>
                         <Link to='/news'>News</Link>
                     </Menu.Item>
                 </Menu>
